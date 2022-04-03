1ecef365808e866f5dbdc44777aa4d7f67001f11
R533-Code <raphaeldibnehmepc@gmail.com>
2022-04-03 20:30:46 +0300

    Added basic files

---
+++ vale_structs/main.cpp
@@ -0,0 +1,7 @@
+#include <comppch.h>
+
+int main(int argc, char** argv)
+{
+	std::cout << "Hello Vale Structs!\n";
+	std::cin.get();
+}
---
+++ vale_structs/utils/comppch.h
@@ -0,0 +1,12 @@
+#ifndef VALE_STRUCTS_COMPPCH
+#define VALE_STRUCTS_COMPPCH
+
+#include <iostream>
+
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+
+#include "config.h"
+
+#endif //VALE_STRUCTS_COMPPCH include guard
