b0a396ac5672c60fdd115abdc91648b2b763451f
R533-Code <raphaeldibnehmepc@gmail.com>
2022-04-04 19:48:56 +0300

    Added include and macro for nanobench

--- vale_structs/main.cpp
+++ vale_structs/main.cpp
@@ -1,5 +1,9 @@
 #include <comppch.h>
 
+//Cannot be put in precompiled header
+#define ANKERL_NANOBENCH_IMPLEMENT
+#include <nanobench.h>
+
 int main(int argc, char** argv)
 {
 	std::cout << "Hello Vale Structs!\n";
