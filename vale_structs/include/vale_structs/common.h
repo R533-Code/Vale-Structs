60aca12adb858c7e67d29cfb383ca9f585c6deea
R533-Code <raphaeldibnehmepc@gmail.com>
2022-04-09 20:59:47 +0300

    Added contiguous_struct_view, which still needs more functionality

--- vale_structs/include/vale_structs/common.h
+++ vale_structs/include/vale_structs/common.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <initializer_list>
+#include <iterator> // For iterator tags
+#include <cstddef>  // For std::ptrdiff_t
 #include <comppch.h>
 
 namespace vale
@@ -18,36 +20,33 @@ namespace vale
 	/// @brief Contains meta-programing utilities
 	namespace details
 	{
-	template<typename T>
+		template<typename T>
 		/// @brief Helper struct to check if a type is [Non]ThreadSafe
 		/// @tparam T The type to check for
-	struct is_thread_safety_policy { static constexpr bool value = false; };
+		struct is_thread_safety_policy { static constexpr bool value = false; };
 
-	template<>
+		template<>
 		/// @brief Overload for ThreadSafe thread safety policy
-	struct is_thread_safety_policy<ThreadSafe> { static constexpr bool value = true; };
+		struct is_thread_safety_policy<ThreadSafe> { static constexpr bool value = true; };
 
-	template<>
+		template<>
 		/// @brief Overload for NonThreadSafe thread safety policy
-	struct is_thread_safety_policy<NonThreadSafe> { static constexpr bool value = true; };
+		struct is_thread_safety_policy<NonThreadSafe> { static constexpr bool value = true; };
 
-	template<typename T>
+		template<typename T>
 		/// @brief helper type to help determine if a type is [Non]ThreadSafe
 		/// @tparam T The type to check for
-	static constexpr bool is_thread_safety_policy_v = is_thread_safety_policy<T>::value;
-
-	struct OptionalBuffer{};
-	struct NonOptionalBuffer{};
-
-	template <typename First, typename... Rest>
+		static constexpr bool is_thread_safety_policy_v = is_thread_safety_policy<T>::value;
+		
+		template <typename First, typename... Rest>
 		/// @brief Checks if a parameter pack is formed by the same type
 		/// @tparam First The first type
 		/// @tparam ...Rest The rest of the parameter pack
 		struct is_parameter_pack_of_same_type
 		{
-		static_assert(std::conjunction_v<std::is_same<First, Rest>...>);
-		using type = First;
-	};
+			static_assert(std::conjunction_v<std::is_same<First, Rest>...>);
+			using type = First;
+		};
 	}
 
 	template<typename T>
@@ -110,21 +109,68 @@ namespace vale
 		/// @return const pointer to the data
 		constexpr const T* data() const noexcept { return ptr; }
 
+		/// @brief Returns the object at 'index', and throws if the index is out of range.
+		/// @param index The index of the object
+		/// @return const reference to the object
 		constexpr const T& operator[](size_t index)
 		{
-			if (index < size)
+			if (index < nb_elem)
 				return ptr[index];
 			throw std::out_of_range("vale::contiguous_struct_view: Index was greater than size!");
 		}
+
+		/// @brief Constructs a struct view with a pointer and a size
+		/// @param ptr Pointer to the beginning of the view
+		/// @param size Number of element pointed to by the view
+		constexpr contiguous_struct_view(const T* ptr, size_t size) noexcept
+			: ptr(ptr), nb_elem(size)
+		{}
+
+		/// @brief Check if the view is empty (points to 0 objects)
+		/// @return true if the view is empty
+		constexpr bool is_empty() const noexcept { return nb_elem == 0; }
+
+		/// @brief Returns the first object in the array
+		/// @return const reference to the first object
+		constexpr const T& front() const
+		{
+			if (!is_empty())
+				return ptr[0];
+			throw std::out_of_range("vale::contiguous_struct_view: View was empty!");
+		}
+		
+		/// @brief Returns the last object in the array
+		/// @return const reference to the last object
+		constexpr const T& back() const
+		{
+			if (!is_empty())
+				return ptr[nb_elem - 1];
+			throw std::out_of_range("vale::contiguous_struct_view: View was empty!");
+		}
+
+		/// @brief Returns an iterator to the beginning of the array
+		/// @return iterator to the beginning of the array
+		constexpr contiguous_iterator<const T> begin()	const noexcept { return ptr; }
+		/// @brief Returns an iterator to the end of the array
+		/// @return iterator to the end of the array
+		constexpr contiguous_iterator<const T> end()	const noexcept { return ptr + nb_elem; }
+
+		/// @brief Returns an const iterator to the beginning of the array
+		/// @return const iterator to the beginning of the array
+		constexpr contiguous_iterator<const T> cbegin()	const noexcept { return ptr; }
+		/// @brief Returns an const iterator to the end of the array
+		/// @return const iterator to the end of the array
+		constexpr contiguous_iterator<const T> cend()	const noexcept { return ptr + nb_elem; }
 	};
 
 	template<typename T>
+	/// @brief writes the content of a view between '{}', separating objects by ','. 
 	std::ostream& operator<<(std::ostream& os, const contiguous_struct_view<T>& var)
 	{
-		os << "{";
-		for (size_t i = 0; i < var.size - 1; i++)
-			os << *(var.ptr + i) << ", ";
-		os << *(var.ptr + var.size - 1) << '}';
+		os << "{";		
+		for (size_t i = 0; i < var.size() - 1; i++)
+			os << *(var.data() + i) << ", ";
+		os << *(var.data() + var.size() - 1) << '}';
 		return os;
 	}
 }
