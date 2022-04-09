530e3326d69ee9da805f5b2bca5ce8fb69a01771
R533-Code <raphaeldibnehmepc@gmail.com>
2022-04-09 20:57:50 +0300

    Added comments and moved meta-programing stuff to ::details

--- vale_structs/include/vale_structs/common.h
+++ vale_structs/include/vale_structs/common.h
@@ -5,29 +5,50 @@
 
 namespace vale
 {
+	/// @brief Thread safety policy which signifies to a struct to use its thread safe implementation
 	struct ThreadSafe {};
+	/// @brief Thread safety policy which signifies to a struct to use its non-thread safe implementation
 	struct NonThreadSafe{};
 
+	/// @brief Buffer policy which signifies to a struct to use an optional buffer
+	struct OptionalBuffer{};
+	/// @brief Buffer policy which signifies to a struct to not use an optional buffer
+	struct NonOptionalBuffer{};
+
+	/// @brief Contains meta-programing utilities
+	namespace details
+	{
 	template<typename T>
+		/// @brief Helper struct to check if a type is [Non]ThreadSafe
+		/// @tparam T The type to check for
 	struct is_thread_safety_policy { static constexpr bool value = false; };
 
 	template<>
+		/// @brief Overload for ThreadSafe thread safety policy
 	struct is_thread_safety_policy<ThreadSafe> { static constexpr bool value = true; };
 
 	template<>
+		/// @brief Overload for NonThreadSafe thread safety policy
 	struct is_thread_safety_policy<NonThreadSafe> { static constexpr bool value = true; };
 
 	template<typename T>
+		/// @brief helper type to help determine if a type is [Non]ThreadSafe
+		/// @tparam T The type to check for
 	static constexpr bool is_thread_safety_policy_v = is_thread_safety_policy<T>::value;
 
 	struct OptionalBuffer{};
 	struct NonOptionalBuffer{};
 
 	template <typename First, typename... Rest>
-	struct is_parameter_pack_of_same_type {
+		/// @brief Checks if a parameter pack is formed by the same type
+		/// @tparam First The first type
+		/// @tparam ...Rest The rest of the parameter pack
+		struct is_parameter_pack_of_same_type
+		{
 		static_assert(std::conjunction_v<std::is_same<First, Rest>...>);
 		using type = First;
 	};
+	}
 
 	template<typename T>
 	struct contiguous_struct_view
