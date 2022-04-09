d998fb1362ba8596e8aa49d86ef5145260e6e7d0
R533-Code <raphaeldibnehmepc@gmail.com>
2022-04-09 21:30:09 +0300

    Added header for vale::variant

---
+++ vale_structs/include/vale_structs/variant.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <vale_structs/common.h>
+
+namespace vale
+{
+	template<typename First, typename... Rest>
+	class variant
+	{
+		//alignas(GREATEST) char buffer[GREATEST]
+	};
+}
