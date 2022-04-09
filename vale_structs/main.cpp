8d6bf483204ea76cd9ea1a0e23796c988b199285
R533-Code <raphaeldibnehmepc@gmail.com>
2022-04-09 21:00:27 +0300

    Added tests for views

--- vale_structs/main.cpp
+++ vale_structs/main.cpp
@@ -6,41 +6,21 @@
 #include <chrono>
 
 #include <vale_structs/array.h>
+#include <array>
+
+template<typename T>
+void testprint(vale::array_view<T> arr)
+{
+	for (auto& i : arr)
+	{
+		std::cout << i << '|';
+	}
+}
+
+using namespace vale;
 
 int main(int argc, char** argv)
 {
-	using namespace std::literals::chrono_literals;
-
-	std::cout << "--------THREAD_SAFE---------\n";
-	for (size_t count = 0; count < 10; count++)
-	{
-		vale::array<int, 10, vale::ThreadSafe> ts_array = {};
-		std::thread thr1([&]() {
-			ts_array.for_each([](int& i) { i = 0; });
-			});
-		std::this_thread::sleep_for(0.2us);
-		ts_array.for_each([](int& i) { i = 1; });
-		
-		thr1.join();
-		std::cout << ts_array << '\n';
-	}
-	std::cout << "------NON_THREAD_SAFE-------\n";
-	for (size_t count = 0; count < 10; count++)
-	{
-		vale::array<int, 10, vale::NonThreadSafe> nts_array = {};
-		std::thread thr1([&]() {
-			for (size_t i = 0; i < nts_array.size(); i++)
-			{
-				nts_array[i] = 0;
-			}
-			});
-		std::this_thread::sleep_for(0.2us);
-		for (size_t i = 0; i < nts_array.size(); i++)
-		{
-			nts_array[i] = 1;
-		}
-
-		thr1.join();
-		std::cout << nts_array << '\n';
-	}
+	vale::array<int, 10> arr = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+	std::cout << arr.to_view(5, 6);
 }
