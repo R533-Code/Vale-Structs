fd71fb0a918452fdafa5c367f6f232ff9a7eabf2
R533-Code <raphaeldibnehmepc@gmail.com>
2022-04-19 21:58:40 +0300

    Added tests for copy/move constructors

--- vale_structs/main.cpp
+++ vale_structs/main.cpp
@@ -21,47 +21,22 @@ struct helper
 	~helper() { std::cout << "~helper called!\n"; }
 };
 
-int main(int argc, char** argv)
+auto func()
 {
-	vale::variant<int, float, helper> arr(1.01f);
-	std::cout << std::boolalpha;
-	PRINT(decltype(arr)::can_be_invalid());
-	std::cout << "destructor complexity: " <<
-		(decltype(arr)::destructor_complexity() == vale::algorithm::linear_complexity
-			? "linear_complexity" : "constant_complexity") << '\n';
-	PRINT(decltype(arr)::is_noexcept_destructible());
-	try
-	{
-		arr.emplace<helper>();
-	}
-	catch(...)
-	{
-		std::cout << "helper constructor throwed an error!\n";
-		if (arr.is_valid())
-			std::cout << "Variant is valid\n";
-		else
-			std::cout << "Variant is invalid\n";
-	}
-	arr = 10;
-	try
-	{
-		std::cout << arr.get<float>() << '\n';		
-	}
-	catch (const std::exception& e)
-	{
-		std::cout << e.what() << '\n';
-	}
+	return vale::variant<int, float, std::string>(std::to_string(10));
+}
 
-	vale::ts_array<int, 10> arr1 = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+int main(int argc, char** argv)
+{	
+	vale::variant<int, float, std::string> arr1(func());
+	std::cout << std::boolalpha;
+	PRINT(decltype(arr1)::alignment());
+	PRINT(decltype(arr1)::buffer_byte_size());
+	PRINT(decltype(arr1)::is_movable());
+	PRINT(decltype(arr1)::is_noexcept_movable());
+	PRINT(decltype(arr1)::is_copyable());
+	PRINT(decltype(arr1)::is_noexcept_copyable());
 	PRINT(arr1);
-	std::random_device rd;
-	std::mt19937 g{ rd() };
-	arr1.pass_iterators(std::shuffle<array_iterator<int>, std::mt19937&>, g);
-	std::shuffle(array_iterator<int>(arr1.buffer), array_iterator<int>(arr1.buffer + arr1.size()), g);
-	PRINT(arr1);
-	arr1.pass_iterators(
-		std::sort<array_iterator<int>, bool(const int&, const int&)>,
-		[](const int& a, const int& b) {return a < b; }
-	);
-	PRINT(arr1);
+	auto arr2 = arr1;
+	PRINT(arr2);
 }
