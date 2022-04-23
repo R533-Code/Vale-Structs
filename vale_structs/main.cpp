3dca74a9799bb9b0dd439de0f6be498c225aebce
R533-Code <raphaeldibnehmepc@gmail.com>
2022-04-23 19:04:53 +0300

    Added tests, everything compiles on MSVC

--- vale_structs/main.cpp
+++ vale_structs/main.cpp
@@ -10,24 +10,28 @@
 
 using namespace vale;
 
+#include <variant>
 #define PRINT(val) std::cout << #val << ": " << val << '\n'
 
 struct helper
 {
 	helper() { std::cout << "helper() called" << std::endl; throw std::exception{}; }
-	helper(const helper&) { std::cout << "helper(const helper&) called\n"; }
+	//helper(const helper&) { std::cout << "helper(const helper&) called\n"; }
+	helper(const helper&) = delete;
 	helper(helper&&) noexcept { std::cout << "helper(helper&&) called\n"; }
 	~helper() { std::cout << "~helper called!\n"; }
 };
 
-auto func()
+std::ostream& operator<<(std::ostream& os, const helper& var)
 {
-	return vale::variant<int, float, std::string>(std::to_string(10));
+	os << "helper";
+	return os;
 }
 
 int main(int argc, char** argv)
-{	
-	vale::variant<int, float, std::string> arr1(std::move(func()));
+{
+	vale::ts_variant<int, float, helper> arr1(10.0f);
+
 	std::cout << std::boolalpha;
 	PRINT(decltype(arr1)::alignment());
 	PRINT(decltype(arr1)::buffer_byte_size());
@@ -36,6 +40,6 @@ int main(int argc, char** argv)
 	PRINT(decltype(arr1)::is_copyable());
 	PRINT(decltype(arr1)::is_noexcept_copyable());
 	PRINT(arr1);
-	auto arr2 = arr1;
-	PRINT(arr2);
+	//auto arr2 = arr1;
+	//PRINT(arr2);
 }
